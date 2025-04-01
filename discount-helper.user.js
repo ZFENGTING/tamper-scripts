37c86ba4936f91d195fc4365181b3f8e9adebdc7
zft <167700174+ZFENGTING@users.noreply.github.com>
2025-04-01 14:31:13 +0200

    Update discount-helper.user.js

--- discount-helper.user.js
+++ discount-helper.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         折扣自动计算助手 v2.0.20250401
+// @name         折扣自动计算助手 v2.0.20250401.1
 // @copyright    2025, ZFT (https://github.com/ZFENGTING)
 // @namespace    https://github.com/ZFENGTING
-// @version      v2.0.20250401
+// @version      v2.0.20250401.1
 // @description  支持普通页和变体页折扣结构，稳定处理所有商品行
 // @match        http://ns71.bosonapp.com/boson/module/sale/sale_reg.php*
 // @updateURL    https://raw.githubusercontent.com/ZFENGTING/tamper-scripts/main/discount-helper.user.js
@@ -207,7 +207,7 @@
                         hasUpdates = true;
                     }
                 }
-                if (usePresale && !isSpecialPrefix && !isNoDiscount) {
+                if (usePresale && !isSpecialPrefix && !isNoDiscount && !isForeign) {
                     const currentPresale = getDiscountValue(presaleCell);
                     if (currentPresale === 0) {
                         await setDiscountValue(presaleCell, 5);
